6fb6993e46a721f9c8d6b88c1a259e6efe083428
Jakob Wegenschimmel <jwegenschimmel@rzl.at>
2018-09-18 11:17:53 +0200

    Test the aswer form @ajcvickers

--- EFCoreOwnedEntityUsageInTwoDifferentEntitiesSample/Program.cs
+++ EFCoreOwnedEntityUsageInTwoDifferentEntitiesSample/Program.cs
@@ -13,6 +13,14 @@ namespace EFCoreOwnedEntityUsageInTwoDifferentEntitiesSample
             WriteLine("\n\nTwo ore more Owned Entities:");
             DbContextWithTwoOrMoreOwnedEntitiesSample();
 
+            WriteLine("\n\n\nPositive Examples, should both work:");
+
+            WriteLine("\n\nOne Owned Entity:");
+            DbContextWithOneOwnedEntityPositiveSample();
+
+            WriteLine("\n\nTwo ore more Owned Entities:");
+            DbContextWithTwoOrMoreOwnedEntitiesPositiveSample();
+
             ReadLine();
         }
 
@@ -40,6 +48,43 @@ namespace EFCoreOwnedEntityUsageInTwoDifferentEntitiesSample
                     entityType is null
                     ? $"The entityType for Type:{typeof(Name)} was not found."  // -> won´t find anything
                     : $"The entityType {entityType.Name} was found."
+                    );
+            }
+        }
+
+        private static void DbContextWithOneOwnedEntityPositiveSample()
+        {
+            using (var context = new DbContextWithOneOwnedEntity())
+            {
+                var authorEntityType = context.Model.FindEntityType(typeof(Author));
+                var authorNameEntityType = context.Model.FindEntityType(typeof(Name), nameof(Author.Name), authorEntityType);
+
+                WriteLine(
+                    authorNameEntityType is null
+                    ? $"The authorNameEntityType for Type:{typeof(Name)} was not found."
+                    : $"The authorNameEntityType {authorNameEntityType.Name} was found."
+                    );
+            }
+        }
+
+        private static void DbContextWithTwoOrMoreOwnedEntitiesPositiveSample()
+        {
+            using (var context = new DbContextWithTwoOrMoreOwnedEntities())
+            {
+                var authorEntityType = context.Model.FindEntityType(typeof(Author));
+                var authorNameEntityType = context.Model.FindEntityType(typeof(Name), nameof(Author.Name), authorEntityType);
+                var customerEntityType = context.Model.FindEntityType(typeof(Customer));
+                var customerNameEntityType = context.Model.FindEntityType(typeof(Name), nameof(Customer.Name), customerEntityType);
+
+                WriteLine(
+                    authorNameEntityType is null
+                    ? $"The authorNameEntityType for Type:{typeof(Name)} was not found."
+                    : $"The authorNameEntityType {authorNameEntityType.Name} was found."
+                    );
+                WriteLine(
+                    customerNameEntityType is null
+                    ? $"The customerNameEntityType for Type:{typeof(Name)} was not found."
+                    : $"The customerNameEntityType {customerNameEntityType.Name} was found."
                     );
             }
         }
